2f0efdc12254cd052ed8b810daa2e34b5f4b20df
Amin Tahseen <51921201+AminTahseen@users.noreply.github.com>
2020-12-11 10:42:05 +0500

    Firebase setup and Login UI Design

--- app/src/main/java/com/masterandroid/backgroundservice/ViewHistoryRecycler.java
+++ app/src/main/java/com/masterandroid/backgroundservice/ViewHistoryRecycler.java
@@ -64,7 +64,8 @@ public class ViewHistoryRecycler extends AppCompatActivity {
             String str[] = type.split(",");
             ArrayList<String> placeType = new ArrayList<String>(Arrays.asList(str));
             place p=new place(
-                    obj.getDouble("placeLongitude"), obj.getDouble("placeLatitude"),
+                    obj.getDouble("placeLongitude"),
+                    obj.getDouble("placeLatitude"),
                     obj.getString("placeAddress"),
                     obj.getString("placeName"),
                     placeType);
