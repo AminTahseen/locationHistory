d9766e86c7555e49ab72746b3778dcdb1d589997
rizwankarim <rizusamnani@gmail.com>
2020-12-16 10:08:01 +0500

    URL Changed

--- app/src/main/java/com/masterandroid/backgroundservice/Api.java
+++ app/src/main/java/com/masterandroid/backgroundservice/Api.java
@@ -1,7 +1,8 @@
 package com.masterandroid.backgroundservice;
 
 public class Api {
-    private static final String ROOT_URL = "https://locationhistoryapi.000webhostapp.com/Api.php?apicall=";
+    //private static final String ROOT_URL = "https://locationhistoryapi.000webhostapp.com/Api.php?apicall=";
+    private static final String ROOT_URL = "https://10.33.32.83/LocationHistory/V1/Api.php?apicall=";
     public static final String URL_CREATE_LIST = ROOT_URL + "createHistory";
     public static final String URL_READ_LIST = ROOT_URL + "getHistory&userId=";
     public static final String URL_UPDATE_LIST = ROOT_URL + "updateHistoryStatus";
