189f992827aaadb15371992c0cc2de93cee6e7d9
Amin Tahseen <51921201+AminTahseen@users.noreply.github.com>
2020-12-17 11:05:39 +0500

    toast added commit

--- app/src/main/java/com/masterandroid/backgroundservice/LocationService.java
+++ app/src/main/java/com/masterandroid/backgroundservice/LocationService.java
@@ -174,12 +174,16 @@ public class LocationService extends Service {
         params.put("visitStatus",VisitStatus);
         params.put("placeTime",placeTime);
 
+        Toast.makeText(this, "Inside storeData", Toast.LENGTH_SHORT).show();
         PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_CREATE_LIST, params, CODE_POST_REQUEST);
         request.execute();
+        Toast.makeText(this, "Inside storeData Executed", Toast.LENGTH_SHORT).show();
 
     }
 
     public void getPlaceSearchDetails(double latitude, double longitude, String input, String inputtime, String fields,String location,String key){
+        Toast.makeText(this, "Inside getPlaceSearchDetails", Toast.LENGTH_SHORT).show();
+
         place_detailsArrayList=new ArrayList<>();
         final ApiInterface apiInterface= ApiClient.getClient().create(ApiInterface.class);
         Call <ResponseModel> call=apiInterface.getPlaceSearch(input,inputtime,fields,location,key);
@@ -188,6 +192,8 @@ public class LocationService extends Service {
             public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
                 if(response.isSuccessful())
                 {
+                    Toast.makeText(getApplicationContext(), "getPlaceSearchDetails Success", Toast.LENGTH_SHORT).show();
+
                     //place myPlace= new place();
                     place_detailsArrayList=response.body().getCandidates();
                     for(int i=0;i<place_detailsArrayList.size();i++)
@@ -207,7 +213,9 @@ public class LocationService extends Service {
                     }
                 }
                 else{
-         // commit
+                    Toast.makeText(getApplicationContext(), "getPlaceSearchDetails Failed", Toast.LENGTH_SHORT).show();
+
+                    // commit
                 }
             }
             @Override
